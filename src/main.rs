4f7e6844038be1375cc649557449ad263175a380
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-06 00:43:31 +0000

    setup test index route, and .env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate actix_web;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+
+// index handler,
+// returns a json response with an ok status
+fn index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().json("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    HttpServer::new(|| App::new().service(web::resource("/").route(web::get().to_async(index))))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
+    //    println!("Hello, world!");
 }
