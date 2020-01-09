0d00dc3aec7ef524e59086f9adf12e52e357e62e
Abey Onalaja <abey.onalaja@gmail.com>
2020-01-09 21:22:43 +0000

    update to user model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,1 +1,23 @@
+use crate::schema::users;
 use chrono::NaiveDateTime;
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
+pub struct User {
+    #[serde(skip)]
+    pub id: i32,
+    pub email: String,
+    pub company: String,
+    #[serde(skip)]
+    pub password: String,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub email: String,
+    pub company: String,
+    pub password: String,
+    pub created_at: NaiveDateTime,
+}
