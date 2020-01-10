4503e69df4d5191bc20271ad73539df4adfba3c2
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-10 00:43:28 +0000

    Complete users model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,5 +1,9 @@
+use crate::errors::MyStoreError;
 use crate::schema::users;
+use bcrypt::{hash, DEFAULT_COST};
+use chrono::Local;
 use chrono::NaiveDateTime;
+use diesel::{PgConnection, RunQueryDsl};
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
 #[table_name = "users"]
@@ -20,4 +24,82 @@ pub struct NewUser {
     pub company: String,
     pub password: String,
     pub created_at: NaiveDateTime,
+}
+
+impl User {
+    pub fn create(
+        register_user: RegisterUser,
+        connection: &PgConnection,
+    ) -> Result<User, MyStoreError> {
+        use diesel::RunQueryDsl;
+
+        Ok(diesel::insert_into(users::table)
+            .values(NewUser {
+                email: register_user.email,
+                company: register_user.company,
+                password: Self.hash_password(register_user.password)?,
+                created_at: Local::now().naive_local(),
+            })
+            .get_result(connection)?)
+    }
+
+    pub fn hash_password(plain: String) -> Result<String, MyStoreError> {
+        Ok(hash(plain, DEFAULT_COST)?)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct RegisterUser {
+    pub email: String,
+    pub company: String,
+    pub password: String,
+    pub password_confirmation: String,
+}
+
+impl RegisterUser {
+    pub fn validate(self) -> Result<RegisterUser, MyStoreError> {
+        if self.password == self.password_confirmation {
+            Ok(self)
+        } else {
+            Err(MyStoreError::PasswordNotMatch(
+                "Password and Password Confirmation does not match".to_string(),
+            ))
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct AuthUser {
+    pub email: String,
+    pub password: String,
+}
+
+impl AuthUser {
+    pub fn login(&self, connection: &pgConnection) -> Result<User, MyStoreError> {
+        use crate::schema::users::dsl::email;
+        use bcrypt::verify;
+        use diesel::ExpressionMethods;
+        use diesel::QueryDsl;
+        use diesel::RunQueryDsl;
+
+        let mut records = users::table
+            .filter(email.eq(&self.email))
+            .load::<User>(connection)?;
+
+        let user = records
+            .pop()
+            .ok_or(MyStoreError::DBError(diesel::result::Error::NotFound))?;
+
+        let verify_password = verify(&self.password, &user.password).map_err(|_error| {
+            MyStoreError::WrongPassword("Wrong password, check again please".to_string())
+        })?;
+
+        if verify_password {
+            Ok(user)
+        } else {
+            Err(MyStoreError::WrongPassword(
+                "Wrong password, check again please".to_string(),
+            ))
+        }
+    }
 }
